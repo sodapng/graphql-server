eee37eb7d68f6ab8df216dde035381ec32d4657b
Kamran <78822102+sodapng@users.noreply.github.com>
2022-07-09 19:20:33 +0000

    feat: users id

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -9,6 +9,21 @@ export class UsersService {
     this.client = axios.create({
       baseURL: 'http://localhost:3004/v1/users',
     });
+
+    this.client.interceptors.response.use((res) => {
+      if (!res.data) {
+        res.data = null;
+        return res;
+      }
+
+      if (res.data.items) {
+        res.data.items = res.data.items.map((el) => ({ ...el, id: el._id }));
+      } else {
+        res.data = { ...res.data, id: res.data._id };
+      }
+
+      return res;
+    });
   }
 
   async login(email: string, password: string) {
